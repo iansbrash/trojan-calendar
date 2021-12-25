5388f3915ce5201169b4e5b9543620de55d764cc
Ian Brash <brash@usc.edu>
2021-12-25 16:22:35 -0600

    Add dropdown with individual assignments for My Grades

--- frontend/components/grades/Grades.tsx
+++ frontend/components/grades/Grades.tsx
@@ -1,5 +1,6 @@
 import React, {
-    FC
+    FC,
+    useState
 } from 'react';
 import ColumnContainer from '../multi/ColumnContainer';
 
@@ -21,30 +22,45 @@ const Grades : FC = () => {
                     chevronColor='text-sky-400'
                     className='MATH-225'
                     highlightColor='hover:bg-sky-100'
+                    bulletColor='text-sky-200'
+                    gradeTextColor='text-sky-700'
+                    gradeBgColor='bg-sky-100'
                 />
 
                 <ClassGradeBox 
                     chevronColor='text-rose-400'
                     className='CSCI-270'
                     highlightColor='hover:bg-rose-100'
+                    bulletColor='text-rose-200'
+                    gradeTextColor='text-rose-700'
+                    gradeBgColor='bg-rose-100'
                 />
 
                 <ClassGradeBox 
                     chevronColor='text-indigo-400'
                     className='BAEP-470'
                     highlightColor='hover:bg-indigo-100'
+                    bulletColor='text-indigo-200'
+                    gradeTextColor='text-indigo-700'
+                    gradeBgColor='bg-indigo-100'
                 />
 
                 <ClassGradeBox 
                     chevronColor='text-emerald-400'
                     className='WRIT-340'
                     highlightColor='hover:bg-emerald-100'
+                    bulletColor='text-emerald-200'
+                    gradeTextColor='text-emerald-700'
+                    gradeBgColor='bg-emerald-100'
                 />
 
                 <ClassGradeBox 
                     chevronColor='text-orange-400'
                     className='CSCI-201'
                     highlightColor='hover:bg-orange-100'
+                    bulletColor='text-orange-200'
+                    gradeTextColor='text-orange-700'
+                    gradeBgColor='bg-orange-100'
                 />
 
             </div>
@@ -52,27 +68,106 @@ const Grades : FC = () => {
     )
 }
 
+
+enum AssignmentStatus {
+    GRADED,
+    NOT_SUBMITTED,
+    NEEDS_GRADING
+}
+
 interface ClassGradeBoxProps {
     className: string,
     chevronColor: string,
-    highlightColor: string
+    highlightColor: string,
+    bulletColor: string,
+    gradeTextColor: string,
+    gradeBgColor: string
 }
 
 const ClassGradeBox : FC<ClassGradeBoxProps> = ({
     className,
     chevronColor,
-    highlightColor
+    highlightColor,
+    bulletColor,
+    gradeTextColor,
+    gradeBgColor
 } : ClassGradeBoxProps) => {
+
+    const [isToggled, setIsToggled] = useState<boolean>(false);
+
+    const data : any = [
+        {
+            assignmentTitle: 'Homework 1',
+            assignmentGrade: '89/100',
+            assignmentStatus: AssignmentStatus.GRADED
+        },
+        {
+            assignmentTitle: 'Homework 2',
+            assignmentGrade: '0/100',
+            assignmentStatus: AssignmentStatus.NOT_SUBMITTED
+        },
+        {
+            assignmentTitle: 'Midterm 1',
+            assignmentGrade: '0/100',
+            assignmentStatus: AssignmentStatus.NEEDS_GRADING
+        }
+    ]
+
+    const getColorBasedOnStatus = (status : AssignmentStatus) => {
+        switch (status) {
+            case AssignmentStatus.GRADED:
+                return "text-green-300";
+            case AssignmentStatus.NEEDS_GRADING:
+                return "text-yellow-300";
+            case AssignmentStatus.NOT_SUBMITTED:
+                return "text-red-300";
+        }
+    }
+
     return (
-        <div className={`cursor-pointer flex flex-row justify-start items-center w-full transition duration-250 ease-in-out ${highlightColor} py-1 rounded-md`}>
-            <div className={`${chevronColor} mx-1`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M9 5l7 7-7 7" />
-                </svg>
+        <div className={`h-auto flex flex-col justify-start items-center w-full `}>
+
+            {/* Header with Chevron */}
+            <div className={`w-full flex flex-row justify-start items-center cursor-pointer transition duration-250 ease-in-out ${highlightColor} py-1 rounded-md`}
+            onClick={() => setIsToggled(!isToggled)}
+            >
+                <div className={`${chevronColor} mx-1 ${isToggled ? 'rotate-90' : 'rotate-0' } transition duration-500 ease-in-out`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M9 5l7 7-7 7" />
+                    </svg>
+                </div>
+                <div className="font-medium text-2xl text-slate-800">
+                    {className}
+                </div>
             </div>
-            <div className="font-medium text-2xl text-slate-800">
-                {className}
+
+            {/* Grades */}
+            <div className={`${isToggled ? 'flex' : 'hidden'} w-full h-auto flex-col justify-start items-center`}>
+                {data.map((gradeObject : any) => 
+                    <div className="w-full flex flex-row justify-between items-center">
+                        <div className="flex flex-row justify-start items-center">
+                            {/* Bullet Point */}
+                            <div className={`font-bold text-3xl mx-2.5 ${getColorBasedOnStatus(gradeObject.assignmentStatus)}`}>
+                                •
+                            </div>
+
+                            {/* Assignment Title */}
+                            <div className="text-lg text-slate-700 font-medium">
+                                {gradeObject.assignmentTitle}
+                            </div>
+                        </div>
+
+                        {/* Grade */}
+                        {/* Assignment Title */}
+                        <div className={`text-lg ${gradeTextColor} font-medium ${gradeBgColor} px-2 rounded-md`}>
+                            {gradeObject.assignmentGrade}
+                        </div>
+
+                        
+                    </div>    
+                )}
             </div>
+            
         </div>
     )
 }
